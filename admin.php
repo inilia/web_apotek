cf0508c498887e1c1b0577755dd2f229a7462f84
inilia <poopayy0412@gmai.com>
2024-07-17 23:02:01 +0700

    web_apotek

--- admin.php
+++ admin.php
@@ -110,12 +110,25 @@
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    <?php
+                                    include "proses/connect.php";
+                                    $query = mysqli_query($conn, "SELECT * FROM tb_user");
+                                    $result = [];
+                                    while ($record = mysqli_fetch_array($query)) {
+                                        $result[] = $record;
+                                    }
+                                    if (empty($result)) {
+                                        echo "<tr><td colspan='6' class='text-center'>Data user tidak ada</td></tr>";
+                                    } else {
+                                        $no = 1;
+                                        foreach ($result as $row) {
+                                            ?>
                                     <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
+                                        <th scope="row"><?php echo $no++ ?></th>
+                                        <td><?php echo $row['nama']; ?></td>
+                                        <td><?php echo $row['username']; ?></td>
+                                        <td><?php echo $row['level']; ?></td>
+                                        <td><?php echo $row['nohp']; ?></td>
                                         <td class="d-flex">
                                             <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal"
                                                 data-bs-target="#viewAdminModal"><i class="bi bi-eye"></i></button>
@@ -126,38 +139,10 @@
                                                 data-bs-target="#deleteAdminModal"><i class="bi bi-trash"></i></button>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>@fat</td>
-                                        <td class="d-flex">
-                                            <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal"
-                                                data-bs-target="#viewAdminModal"><i class="bi bi-eye"></i></button>
-                                            <button class="btn btn-warning btn-sm me-1" data-bs-toggle="modal"
-                                                data-bs-target="#editAdminModal"><i
-                                                    class="bi bi-pencil-square"></i></button>
-                                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                data-bs-target="#deleteAdminModal"><i class="bi bi-trash"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry the Bird</td>
-                                        <td>@twitter</td>
-                                        <td>@twitter</td>
-                                        <td>@twitter</td>
-                                        <td class="d-flex">
-                                            <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal"
-                                                data-bs-target="#viewAdminModal"><i class="bi bi-eye"></i></button>
-                                            <button class="btn btn-warning btn-sm me-1" data-bs-toggle="modal"
-                                                data-bs-target="#editAdminModal"><i
-                                                    class="bi bi-pencil-square"></i></button>
-                                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                data-bs-target="#deleteAdminModal"><i class="bi bi-trash"></i></button>
-                                        </td>
-                                    </tr>
+                                    <?php
+                                        }
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -178,28 +163,62 @@
                     </div>
                     <div class="modal-body">
                         <!-- Form untuk tambah admin -->
-                        <form>
-                            <div class="mb-3">
-                                <label for="nama" class="form-label">Nama</label>
-                                <input type="text" class="form-control" id="nama">
+                        <form action="proses/proses_input_user.php">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="floatingInput"
+                                            placeholder="Your Name">
+                                        <label for="floatingInput">Nama</label>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <input type="email" class="form-control" id="floatingInput"
+                                            placeholder="name@example.com">
+                                        <label for="floatingInput">Username</label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="form-floating mb-3">
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected hidden>Pilih Level User</option>
+                                            <option value="1">Admin</option>
+                                            <option value="2">Kasir</option>
+                                            <option value="3">Pelayan</option>
+                                            <option value="4">Gudang</option>
+                                        </select>
+                                        <label for="floatingInput">Level User</label>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8">
+                                    <div class="form-floating mb-3">
+                                        <input type="number" class="form-control" id="floatingInput"
+                                            placeholder="08xxxxx">
+                                        <label for="floatingInput">No HP</label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="level" class="form-label">Level</label>
-                                <input type="text" class="form-control" id="level">
-                            </div>
-                            <div class="mb-3">
-                                <label for="no_telp" class="form-label">No Telp</label>
-                                <input type="text" class="form-control" id="no_telp">
-                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control" id="floatingInput"
+                                            placeholder="password" disabled value="12345">
+                                        <label for="floatingPassword">Password</label>
+                                    </div>
+                                </div>
+                                <div class="form-floating">
+                                    <textarea class="from-control" name="" id="" style="height: 100px"></textarea>
+                                    <label for="floatingInput">Alamat</label>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save
+                                        changes</button>
                         </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
@@ -212,7 +231,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="viewAdminModalLabel">View Admin</h1>
+                        <h1 class="modal-title fs-5" id="viewAdminModalLabel">Daftar Admin</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
